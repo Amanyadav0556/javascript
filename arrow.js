b8de485b35d2a8c89ce6a91e873387aadd3d6cc2
Aman Yadav <aman2004yad@gmail.com>
2026-02-10 13:03:08 +0530

    Initial commit

---
+++ arrow.js
@@ -0,0 +1,9 @@
+const person =() => {
+    const name = 'ram';
+    const age = 23;
+    return  "Name: " + name + ", Age: " + age;
+        
+    
+    
+}
+export default person;
---
+++ function.js
@@ -0,0 +1,5 @@
+export default function person() {
+    const name = 'ram';
+    const age = 23;
+    return  "Name: " + name + ", Age: " + age;
+}
