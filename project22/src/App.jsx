073cc11f62e47d0a669196b293783ca5d0fd52cc
Aman Yadav <aman2004yad@gmail.com>
2026-04-20 13:07:48 +0530

    project21 22

---
+++ project22/src/App.jsx
@@ -0,0 +1,23 @@
+ import React, { useRef, useState } from "react";
+
+function App() {
+  const countRef = useRef(0);
+  const [stateCount, setStateCount] = useState(0);
+
+  return (
+    <div>
+      <h3>Ref Count: {countRef.current}</h3>
+      <h3>State Count: {stateCount}</h3>
+
+      <button onClick={() => countRef.current++}>
+        Increase Ref (No Re-render)
+      </button>
+
+      <button onClick={() => setStateCount(stateCount + 1)}>
+        Increase State (Re-render)
+      </button>
+    </div>
+  );
+}
+
+export default App;
